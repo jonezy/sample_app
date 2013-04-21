e2c902871dde54275a7b8b90dccd17cef03575c4
Chris Jones <chris@jonezy.org>
2013-04-21 17:39:14 -0400

    Initial commit
    Signed-off-by: Chris Jones <chris@jonezy.org>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '54edd9f53c6277af44194a65f62c93a34749d1962bbf3c1dcaddd85bc9be446b17c41fda2f35a8b5ea671f279072d199c66c5266cfd00ff5dd211a1a7221245e'
