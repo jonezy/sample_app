1b2c11050e517f3d060dc0097e4ed0d1acb5a599
Chris Jones <chris@jonezy.org>
2013-05-01 18:59:08 -0400

    finished chapter 6 examples (code)
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -97,6 +97,7 @@ describe User do
       before { @user.password_confirmation = nil }
       it { should_not be_valid }
   end
+
   describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
@@ -110,6 +111,16 @@ describe User do
 
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
+    end
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
     end
   end
 end
