f8ac0cc8b232db0b86bd6abc8203a6402ad918e8
Chris Jones <chris@jonezy.org>
2013-05-02 19:51:34 -0400

    finished up chapter 7
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,12 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before {click_button submit}
+        it {should have_selector('title', text: 'Sign up')}
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -39,6 +45,7 @@ describe "UserPages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+
       end
     end
   end
